9e9bd89286c3394ca66c3471e2f235ecfb124220
mrmarioi <ivanovic.mario@gmail.com>
2022-06-24 18:02:57 +0200

    :construction: update function to remove class :construction:

--- app.js
+++ app.js
@@ -24,16 +24,16 @@ const table =
 <td id="conseqScale"></td>
 <td id="conseqScale"></td>
 <td></td>
-<td class="numberScale">1</td>
-<td class="numberScale">2</td>
-<td class="numberScale">3</td>
-<td class="numberScale">4</td>
-<td class="numberScale">5</td>
+<td id="numScale" class="numberScale">1</td>
+<td  id="numScale" class="numberScale">2</td>
+<td  id="numScale" class="numberScale">3</td>
+<td  id="numScale" class="numberScale">4</td>
+<td  id="numScale" class="numberScale">5</td>
 </tr>
 <tr>
 <td class="probability">Frequent (once a week)</td>
 <td class="probaTitle" rowspan="5">Probability</td>
-<td class="numberProba">5</td>
+<td id="numProba" class="numberProba">5</td>
 <td id="51" class ="cell data"></td>
 <td id="52" class ="cell orange"></td>
 <td id="53" class ="cell red"></td>
@@ -42,7 +42,7 @@ const table =
 </tr>
 <tr>
 <td class="probability">Possible (once 6 months)</td>
-<td class="numberProba">4</td>
+<td id="numProba" class="numberProba">4</td>
 <td id="41" class ="cell data"></td>
 <td id="42" class ="cell data"></td>
 <td id="43" class ="cell orange"></td>
@@ -51,7 +51,7 @@ const table =
 </tr>
 <tr>
 <td class="probability">Unusual (once 3 years)</td>
-<td class="numberProba">3</td>
+<td id="numProba" class="numberProba">3</td>
 <td id="31" class ="cell data"></td>
 <td id="32" class ="cell data"></td>
 <td id="33" class ="cell orange"></td>
@@ -60,7 +60,7 @@ const table =
 </tr>
 <tr>
 <td class="probability">Unlikely (once 10 years)</td>
-<td class="numberProba">2</td>
+<td id="numProba" class="numberProba">2</td>
 <td id="21" class ="cell data"></td>
 <td id="22" class ="cell data"></td>
 <td id="23" class ="cell data"></td>
@@ -69,7 +69,7 @@ const table =
 </tr>
 <tr>
 <td class="probability">Highly unlikely (once in career)</td>
-<td class="numberProba">1</td>
+<td id="numProba" class="numberProba">1</td>
 <td id="11" class =" cell data"></td>
 <td id="12" class =" cell data"></td>
 <td id="13" class =" cell data"></td>
@@ -81,70 +81,13 @@ const table =
 
 div.innerHTML = table;
 
-// Number Scale
-const scaleOne = document.querySelectorAll('#numScale')[0];
-const scaleTwo = document.querySelectorAll('#numScale')[1];
-const scaleThree = document.querySelectorAll('#numScale')[2];
-const scaleFour = document.querySelectorAll('#numScale')[3];
-const scaleFive = document.querySelectorAll('#numScale')[4];
-
-// Number probability
-const probaFive = document.querySelectorAll('#numProba')[0];
-const probaFour = document.querySelectorAll('#numProba')[1];
-const probaThree = document.querySelectorAll('#numProba')[2];
-const probaTwo = document.querySelectorAll('#numProba')[3];
-const ProbaOne = document.querySelectorAll('#numProba')[4];
-
-// Cells
-// Row => from 5 to 1 (up to down).
-// Row 5
-const cellFirstRowf = document.getElementById('51');
-const cellFirstRows = document.getElementById('52');
-const cellFirstRowt = document.getElementById('53');
-const cellFirstRowfr = document.getElementById('54');
-const cellFirstRowfv = document.getElementById('55');
-
-// Row 4
-const cellSecondRowf = document.getElementById('41');
-const cellSecondRows = document.getElementById('42');
-const cellSecondRowt = document.getElementById('43');
-const cellSecondRowfr = document.getElementById('44');
-const cellSecondRowfv = document.getElementById('45');
-
-//Row 3
-const cellThirdRowf = document.getElementById('31');
-const cellThirdRows = document.getElementById('32');
-const cellThirdRowt = document.getElementById('33');
-const cellThirdRowfr = document.getElementById('34');
-const cellThirdRowfv = document.getElementById('35');
-
-//Row 2
-const cellFourthRowf = document.getElementById('21');
-const cellFourthRows = document.getElementById('22');
-const cellFourthRowt = document.getElementById('23');
-const cellFourthRowfr = document.getElementById('24');
-const cellFourthRowfv = document.getElementById('25');
-
-//Row1
-const cellFifthRowf = document.getElementById('11');
-const cellFifthRows = document.getElementById('12');
-const cellFifthRowt = document.getElementById('13');
-const cellFifthRowfr = document.getElementById('14');
-const cellFifthRowfv = document.getElementById('15');
-
-function cellCase(proba, scale) {
-    if ((proba === probaFive) && (scale === scaleOne)) {
-        return cellFirstRowf.classList.add('pulse');
-    } else
-        if (proba === proba5 && scale === scale2) {
-            return cell2.classList.add("pulse");
-
-        }
-    console.log("2", proba, scale, cell1)
+function cellFocus(proba, scale) {
+    let intersection = proba + scale;
+    let cell = document.getElementById(intersection);
+    if (document.querySelectorAll('pulse').length) {
+        document.querySelectorAll('pulse')[0].classList.remove('pulse')
+    }
+    cell.classList.add('pulse');
 }
-cellCase(5, 2);
 
-// const cellules = document.querySelectorAll(".cell");
-// let items = Array.from(cellules).map(elem => {
-//     return elem.id
-// });
+cellFocus(5, 2);
